ff1f98ed7da2d85ca0f0761813d37eb26b9a99e3
mundostr <idux.net@gmail.com>
2019-05-30 19:09:56 -0300

    Sistema de archivos SPIFFS

---
+++ ejercicios/esp32_spiffs/esp32_spiffs.ino
@@ -0,0 +1,69 @@
+/*
+  https://github.com/me-no-dev/arduino-esp32fs-plugin
+  https://randomnerdtutorials.com/install-esp32-filesystem-uploader-arduino-ide/
+  https://randomnerdtutorials.com/esp32-web-server-spiffs-spi-flash-file-system/
+
+  Ejemplo de uso del sistema SPIFFS para almacenar y gestionar archivos en el propio ESP32
+  Para comenzar a utilizarlo -como cualquier sistema de archivos- es necesario LA PRIMERA VEZ,
+  definir y formatear una partición, lo cual se realiza siguiendo estos pasos:
+
+  1) Crear un sketch nuevo y guardarlo.
+  2) Presionar CTRL + K para abrir el directorio de ese sketch
+  3) Crear en ese lugar un subdirectorio llamado "data" (sin las comillas)
+  4) Colocar dentro de data cualquier archivo que se quiera almacenar en el SPIFFS, por ejemplo un index.html
+  5) Ir a Herramientas -> ESP32 Sketch Data Upload para cargar. Este paso iniciará el formateo y
+  preparará el sistema para futuras cargas.
+*/
+
+#include <SPIFFS.h>
+
+void listarDir(char *dir) {
+  static int archivos = 0;
+
+  File raiz = SPIFFS.open(dir);
+  File archivo = raiz.openNextFile();
+
+  Serial.println("Listado de " + (String)dir);
+  while (archivo) {
+    archivos++;
+    Serial.print("Archivo: ");
+    Serial.println(archivo.name());
+    archivo = raiz.openNextFile();
+  }
+
+  if (archivos == 0) {
+    Serial.println("No hay archivos");
+  }
+}
+
+void leerArchivo(char *arc) {
+  Serial.println("Lectura de " + (String)arc);
+
+  File archivo = SPIFFS.open(arc);
+  if (!archivo) {
+    Serial.println("Error al abrir el archivo para lectura");
+    return;
+  }
+
+  Serial.println("Contenido:");
+  while (archivo.available()) {
+    Serial.write(archivo.read());
+  }
+  archivo.close();
+}
+
+void setup() {
+  Serial.begin(115200);
+
+  if (!SPIFFS.begin(true)) {
+    Serial.println("ERROR al montar SPIFFS");
+    return;
+  }
+  Serial.println("SPIFFS montado (" + String(SPIFFS.usedBytes()) + " bytes usados)");
+
+  listarDir("/");
+  // leerArchivo("/menu.html");
+}
+
+void loop() {
+}
