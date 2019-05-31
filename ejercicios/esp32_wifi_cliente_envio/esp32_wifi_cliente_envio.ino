6493bf0812f8fd84e4f25c940f6e3789351f0419
mundostr <idux.net@gmail.com>
2019-05-30 22:53:06 -0300

    Actualización comentarios wf_cl_envio

--- ejercicios/esp32_wifi_cliente_envio/esp32_wifi_cliente_envio.ino
+++ ejercicios/esp32_wifi_cliente_envio/esp32_wifi_cliente_envio.ino
@@ -1,3 +1,7 @@
+/*
+  Ejemplo de envío simple http a servidor externo utilizando HTTPClient
+*/
+
 #include <WiFi.h>
 #include <HTTPClient.h>
 
@@ -10,6 +14,7 @@ void setup() {
 }
 
 void loop() {
+  // Cada 30 segs chequeamos si estamos online en la conexión WF, si es así sensamos y enviamos, sino reconectamos
   if (WiFi.status() == WL_CONNECTED) {
     sensarYEnviar();
   } else {
@@ -31,18 +36,30 @@ void conectarWF() {
 }
 
 void sensarYEnviar() {
+  // Dentro de la propia función de sensado y envío, generamos el objeto para la conexión http
   HTTPClient http;
 
-  http.begin("http://dweet.io/dweet/for/itec_sensor_44?m=" + String(millis()));
-  
+  // Nos conectamos en este caso a un servidor Dweet para enviar rápidamente un dato
+  // sin necesidad de registrar ninguna cuenta, validarnos, etc.
+  // itec_sensor_44 y el argumento valor1 son por supuesto generados a nuestro gusto,
+  // podemos colocar cualquier nombre de dweet y funcionará sin problemas, excepto lógicamente
+  // que estuviese siendo ya aplicado por otro usuario en ese momento.
+  http.begin("http://dweet.io/dweet/for/itec_sensor_44?valor1=" + String(millis()));
+
+  // Para leer por navegador el último dato recibido por el dweet, empleamos la dirección
+  // https://dweet.io/get/latest/dweet/for/itec_sensor_44
+
   int codigoRespuesta = http.GET();
   Serial.println("Respuesta: " + String(codigoRespuesta));
   if (codigoRespuesta > 0) {
+    // Si el código de respuesta es > 0, significa que la petición fue procesada
     String respuesta = http.getString();
     Serial.println(respuesta);
   } else {
+    // Si es negativo, hubo un error
     Serial.print("ERROR al enviar solicitud: ");
   }
 
+  // Bajamos el cliente hasta la próxima iteracción
   http.end();
 }
