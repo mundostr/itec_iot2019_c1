875b3ed298ec1ca71d9e1fcd3d9ace94708ce01a
mundostr <idux.net@gmail.com>
2019-06-03 22:36:41 -0300

    Ajustes sensor Dallas

--- ejercicios/esp32_sensor_dallas/esp32_sensor_dallas.ino
+++ ejercicios/esp32_sensor_dallas/esp32_sensor_dallas.ino
@@ -6,44 +6,22 @@
 #include <Ticker.h>
 #include <OneWire.h>
 #include <DallasTemperature.h>
-#include <Adafruit_NeoPixel.h>
 
-const byte ONEWIRE = 15;
-const byte MULTILED = 16;
-const int FREC_SENSOR = 250;
-const byte LIMITE_TEMP = 29;
+const byte LED = 2;
+const byte ONEWIRE = 23;
+const int FREC_SENSOR = 100;
+const byte LIMITE_TEMP = 25;
 const char* REDWF = "APIOT";
 const char* CLAVE = "arduino2019";
-enum condiciones {BAJA, NORMAL, ALTA };
 
 Ticker ctrlSensor;
 OneWire oneWire(ONEWIRE);
 DallasTemperature sensors(&oneWire);
-Adafruit_NeoPixel ledM = Adafruit_NeoPixel(1, MULTILED, NEO_GRB + NEO_KHZ800);
-condiciones condicionTemperatura = NORMAL;
-condiciones condicionTemperaturaPrevia = NORMAL;
-
-void encenderLed(byte led, condiciones temperatura) {
-  ledM.clear();
-  switch (temperatura) {
-    case NORMAL:
-      ledM.setPixelColor(led, 255, 0, 0);
-      break;
-
-    case ALTA:
-      ledM.setPixelColor(led, 0, 255, 0);
-      break;
-  }
-  ledM.show();
-}
 
 void conectarWiFi() {
-  encenderLed(0, ALTA);
   WiFi.begin(REDWF, CLAVE);
-
   Serial.println("Conectando a " + String(REDWF));
   while (WiFi.status() != WL_CONNECTED) {}
-  encenderLed(0, NORMAL);
   Serial.println("OK! ");
   Serial.println(WiFi.macAddress());
   Serial.println(WiFi.localIP());
@@ -52,18 +30,14 @@ void conectarWiFi() {
 void leerSensor() {
   sensors.requestTemperatures();
   float temperatura = sensors.getTempCByIndex(0);
-  temperatura > LIMITE_TEMP ? condicionTemperatura = ALTA : condicionTemperatura = NORMAL;
-  if (condicionTemperatura != condicionTemperaturaPrevia) {
-    encenderLed(0, condicionTemperatura);
-    condicionTemperaturaPrevia = condicionTemperatura;
-  }
+  temperatura > LIMITE_TEMP ? digitalWrite(LED, HIGH) : digitalWrite(LED, LOW);
   Serial.println(temperatura);
 }
 
 void setup() {
   Serial.begin(115200);
   Serial.println("SISTEMA INICIADO");
-  ledM.begin();
+  pinMode(LED, OUTPUT);
   sensors.begin();
   conectarWiFi();
   ctrlSensor.attach_ms(FREC_SENSOR, leerSensor);
