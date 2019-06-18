254e0389a9b4f5c20da06faabbfd186bb04cdc50
mundostr <idux.net@gmail.com>
2019-06-18 20:26:37 -0300

    Ejemplo dual MQTT publica y suscribe

---
+++ ejercicios/esp32_wifi_mqtt_dual/esp32_wifi_mqtt_dual.ino
@@ -0,0 +1,78 @@
+#include <WiFi.h>
+#include <PubSubClient.h>
+
+const byte PIN_LED = 2;
+const byte PIN_SENSOR = 23;
+
+const char *REDWF = "itec";
+const char *CLAVEWF = "itec2019";
+const char* SERVIDORMQTT = "broker.hivemq.com";
+const int PUERTOMQTT = 1883;
+const char* USUARIOMQTT = "";
+const char* CLAVEMQTT = "";
+
+WiFiClient cliente;
+PubSubClient mqtt(cliente);
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(PIN_LED, OUTPUT);
+  digitalWrite(PIN_LED, LOW);
+
+  Serial.println("SISTEMA INICIADO");
+  Serial.println();
+
+  conectarWiFi();
+  conectarMqtt();
+  suscribir();
+}
+
+void loop() {
+  mqtt.loop();
+}
+
+void conectarWiFi() {
+  Serial.println("Conectando a red Wifi");
+  WiFi.begin(REDWF, CLAVEWF);
+
+  while (WiFi.status() != WL_CONNECTED) {}
+  Serial.println("Wifi conectado");
+  Serial.println(WiFi.macAddress());
+  Serial.println(WiFi.localIP());
+}
+
+void conectarMqtt() {
+  mqtt.setServer(SERVIDORMQTT, PUERTOMQTT);
+
+  long nroRandom = random(10000);
+  while (!mqtt.connected()) {
+    if (mqtt.connect("itec_cperren", USUARIOMQTT, CLAVEMQTT)) {
+      digitalWrite(PIN_LED, HIGH);
+      Serial.println("Mqtt conectado");
+    } else {
+      Serial.print("Mqtt: ERROR, ");
+      Serial.println(mqtt.state());
+      delay(3000);
+    }
+  }
+}
+
+void enviarMqtt() {
+  String cadena = "{\"timestamp\": " + String(millis()) + ", \"valor\": 23 }";
+  mqtt.publish("cperren/topico03", cadena.c_str(), false); // true / false retained
+  Serial.println("Mensaje enviado (" + cadena + ")");
+}
+
+void suscribir() {
+  mqtt.setCallback(cbMensajes);
+  mqtt.subscribe("cperren/m01");
+}
+
+void cbMensajes(char* topico, byte* payload, unsigned int length) {
+  Serial.println(topico);
+
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+}
